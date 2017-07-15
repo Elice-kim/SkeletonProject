f6e22d34427eaf85c0d93d09f61504dfe227c305
Elice-kim <grit_julia@naver.com>
2017-07-15 20:47:10 +0900

    add RestClient in module

--- SkeletonPJ/app/src/main/java/com/rxandroid/elice/skeletonpj/module/RestClient.java
+++ SkeletonPJ/app/src/main/java/com/rxandroid/elice/skeletonpj/module/RestClient.java
@@ -1,10 +1,37 @@
 package com.rxandroid.elice.skeletonpj.module;
 
+import com.rxandroid.elice.skeletonpj.repository.ImageRepository;
+import com.rxandroid.elice.skeletonpj.util.constant.WoConstant;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 /**
  * Created by elice.kim on 2017. 7. 15..
  */
 
 public class RestClient {
 
-    
+    public ImageRepository getImageRepository(){
+        return new Retrofit.Builder()
+                .baseUrl(WoConstant.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .client(getOkHttpClient())
+                .build().create(ImageRepository.class);
+    }
+
+    private OkHttpClient getOkHttpClient() {
+        return new OkHttpClient.Builder()
+                .connectTimeout(59, TimeUnit.SECONDS)
+                .addInterceptor(new HttpLoggingInterceptor())
+                .writeTimeout(59, TimeUnit.SECONDS)
+                .readTimeout(59, TimeUnit.SECONDS)
+                .build();
+    }
 }
---
+++ SkeletonPJ/app/src/main/java/com/rxandroid/elice/skeletonpj/util/constant/WoConstant.java
@@ -0,0 +1,10 @@
+package com.rxandroid.elice.skeletonpj.util.constant;
+
+/**
+ * Created by elice.kim on 2017. 7. 15..
+ */
+
+public class WoConstant {
+
+    public static final String BASE_URL = "https://apis.daum.net/search/";
+}
