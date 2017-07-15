db867a679e2f90f01eb407217330ce4734859217
Elice-kim <grit_julia@naver.com>
2017-07-15 19:51:17 +0900

    add Library and packaging

--- SkeletonPJ/app/src/main/java/com/rxandroid/elice/skeletonpj/activity/MainActivity.java
+++ SkeletonPJ/app/src/main/java/com/rxandroid/elice/skeletonpj/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.rxandroid.elice.skeletonpj;
+package com.rxandroid.elice.skeletonpj.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.rxandroid.elice.skeletonpj.R;
 
 public class MainActivity extends AppCompatActivity {
 
